7e7f787ca722a58c6d75aa29521cce622f8a789c
Henry (Hendra) Gozali <123764602+goz88hen@users.noreply.github.com>
2023-04-10 22:15:23 -0700

    Completed excercise step 1.

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace Classes
+﻿using Classes;
+using System.Xml.Schema;
+
+namespace Classes
 {
-    public class Program
+
+    class Program
     {
+       
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car myCar = new Car();
+            myCar.Make = "Audi";
+            myCar.Model = "Q3";
+            myCar.Year = 2020;
+
+            var Audi = new Car()
+            {
+                Make = "Audi",
+                Model = "A4",
+                Year = 2020 
+            };
+
+            var Honda = new Car("Honda", "Civic", 2022);
+
+            var carList = new List<Car>() { myCar, Audi, Honda, new Car (), new Car () };
+           
+            foreach(var vehicle in carList)
+            {
+                Console.WriteLine($"{ vehicle.Make} { vehicle.Model} {vehicle.Year}");
+            }
         }
     }
 }
